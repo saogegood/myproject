bb0fa82d49662e91e211b0b070d23e3dc3718cea
Devin <saogegood@163.com>
2023-04-01 08:10:51 +0800

    Changes

--- myapp/views.py
+++ myapp/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 # 定义视图函数 hello，接收一个请求对象作为参数
 def hello(request):
     # 返回一个 HTTP 响应对象，内容为 "Hello, World!"
-    return HttpResponse("Hello world1234!")
+    return HttpResponse("Hello world14!")
